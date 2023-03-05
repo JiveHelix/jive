3a6aec5b61d1b39063d9ea177770a4a155ea557a
Jive Helix <jivehelix@gmail.com>
2023-03-04 22:47:27 -0700

    Added ScopedCountFlag.

--- jive/scope_flag.h
+++ jive/scope_flag.h
@@ -1,14 +1,18 @@
 #pragma once
 
 
+#include <atomic>
+
+
 namespace jive
 {
 
 
+template<typename Flag = bool>
 class ScopeFlag
 {
 public:
-    ScopeFlag(bool &flag)
+    ScopeFlag(Flag &flag)
         :
         flag_(flag)
     {
@@ -26,7 +30,76 @@ public:
     ScopeFlag & operator=(ScopeFlag &&) = delete;
 
 private:
-    bool &flag_;
+    Flag &flag_;
+};
+
+
+template<typename T>
+class CountFlag
+{
+public:
+    CountFlag()
+        :
+        count_{}
+    {
+
+    }
+
+    operator bool () const
+    {
+        return this->count_ != 0;
+    }
+
+    CountFlag & operator++()
+    {
+        ++this->count_;
+        return *this;
+    }
+
+    CountFlag & operator--()
+    {
+        if (this->count_ == 0)
+        {
+            throw std::logic_error("Count is 0");
+        }
+
+        --this->count_;
+        return *this;
+    }
+
+    T GetCount() const
+    {
+        return this->count_;
+    }
+
+private:
+    std::atomic<T> count_;
+};
+
+
+template<typename T>
+class ScopedCountFlag
+{
+public:
+    ScopedCountFlag(CountFlag<T> &flag)
+        :
+        flag_(flag)
+    {
+        ++this->flag_;
+    }
+
+    ~ScopedCountFlag()
+    {
+        --this->flag_;
+    }
+
+    ScopedCountFlag(const ScopedCountFlag &) = delete;
+    ScopedCountFlag(ScopedCountFlag &&) = delete;
+    ScopedCountFlag & operator=(const ScopedCountFlag &) = delete;
+    ScopedCountFlag & operator=(ScopedCountFlag &&) = delete;
+
+private:
+    CountFlag<T> &flag_;
 };
 
 
