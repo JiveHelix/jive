b6972b36e28a12327d4fab911a5de1dc45e833ce
Jive Helix <jivehelix@gmail.com>
2025-07-12 14:26:51 -0600

    Created helpers for std::vector erase/insert.

---
+++ jive/vector.h
@@ -0,0 +1,63 @@
+#pragma once
+
+
+#include <vector>
+#include <limits>
+
+
+namespace jive
+{
+
+
+template<typename T>
+auto IndexIterator(std::vector<T> &vector, size_t index)
+{
+    assert(index <= std::numeric_limits<ssize_t>::max());
+    assert(index < vector.size());
+
+    return std::begin(vector) + static_cast<ssize_t>(index);
+}
+
+template<typename T>
+auto SafeEraseIterator(std::vector<T> &vector, size_t index)
+{
+    assert(index <= std::numeric_limits<ssize_t>::max());
+
+    if (index >= vector.size())
+    {
+        throw std::out_of_range("element does not exist");
+    }
+
+    return std::begin(vector) + static_cast<ssize_t>(index);
+}
+
+
+template<typename T>
+auto SafeInsertIterator(std::vector<T> &vector, size_t index)
+{
+    assert(index <= std::numeric_limits<ssize_t>::max());
+
+    if (index > vector.size())
+    {
+        throw std::out_of_range("element does not exist");
+    }
+
+    return std::begin(vector) + static_cast<ssize_t>(index);
+}
+
+
+template<typename T>
+void Erase(std::vector<T> &vector, size_t index)
+{
+    vector.erase(IndexIterator(vector, index));
+}
+
+
+template<typename T>
+void SafeErase(std::vector<T> &vector, size_t index)
+{
+    vector.erase(SafeEraseIterator(vector, index));
+}
+
+
+} // end namespace jive
